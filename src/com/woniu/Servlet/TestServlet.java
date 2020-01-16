99c0223bc40a17ed3522eb4eef722dd9ad996fbe
zhang <zhangchend099@sina.com>
2020-01-16 20:24:05 +0800

    123第一次提交

--- src/com/woniu/Servlet/TestServlet.java
+++ src/com/woniu/Servlet/TestServlet.java
@@ -28,6 +28,7 @@ public class TestServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("aaaaaaa");
+		System.out.println("bbbbbbb");
 	}
 
 	/**
